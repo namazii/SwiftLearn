a4083615198b52f987bd46e03f65bb6dabf43626
namazii <nazar383875@gmail.com>
2022-08-23 22:08:01 +0300

    feat(json): improve model json

--- SwiftLearn/Models/JSON.swift
+++ SwiftLearn/Models/JSON.swift
@@ -7,14 +7,22 @@
 
 import Foundation
 
+// MARK: - Questions
 struct QuestionsJSON: Codable {
+    let topicCount: Int
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
     let id: Int
     let text: String
-    let code: String
-    let type: String
+    let code: String?
+    let type, topic: String
     let answers: [AnswerJSON]
 }
 
+// MARK: - Answer
 struct AnswerJSON: Codable {
     let id: Int
     let status: Bool
